dc882ae1a0104fc183beca781e5a16581bfc1caf
Emmanuel Rukundo <emmanuelrukundo@Emmanuels-MacBook-Pro-2.local>
2019-12-27 13:05:42 +0200

    setup the project

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mangoURI');
+
+ const connectDb = async () => {
+     try {
+         mongoose.connect(db, { 
+             useUnifiedTopology: true, 
+             useNewUrlParser: true,
+             userCreateIndex: true
+            });
+         console.log('MongoDB Connected..');
+     } catch(err){
+          console.error(err.message);
+          //Exit process with failure
+          process.exit( 1);
+     }
+ };
+
+ module.exports = connectDb;
