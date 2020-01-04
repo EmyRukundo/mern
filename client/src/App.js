769ef8d764d7be8cd31e672f559877990dc69d70
Emmanuel Rukundo <emmanuelrukundo@Emmanuels-MacBook-Pro-2.local>
2020-01-04 16:02:38 +0200

    auth reducer && register action

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,22 @@ import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const  App = () =>  {
   return (
+    <Provider store={store}>
     <Router>
    <Fragment>
      <NavBar />
      <Route exact path='/' component={Landing} />
      <section className="container">
+       <Alert />
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
@@ -20,6 +27,7 @@ const  App = () =>  {
      </section>
    </Fragment>
    </Router>
+   </Provider>
   );
 }
 
